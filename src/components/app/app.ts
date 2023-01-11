62f0d397d77b55dc0c6221be5b03366f1fab85e6
jahanbaev <sjahanbaev@gmail.com>
2023-01-11 23:09:20 +0500

    feat: ts files added

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,6 +2,8 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -9,7 +11,7 @@ class App {
 
     start() {
         document
-            .querySelector('.sources')
+            .querySelector('.sources')!
             .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
